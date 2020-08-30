920587910cc067c80c926686f85ed28a173eed71
Glaydson Prado <glaydsonp@hotmail.com>
2020-08-30 11:34:18 -0300

    feat: creating shared components

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  const hello = 'Hello World';
+  return (
+    <>
+      <div>{hello}</div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  const hello = 'Hello World';
+  return (
+    <>
+      <div>{hello}</div>
+    </>
+  );
+};
+
+export default Header;
